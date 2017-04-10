30d030c719be7c8bedfdd94f742f321ee7e50ef6
lqqppl <1277601297@qq.com>
2017-04-10 15:49:49 +0800

    Add elementUI

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import resource from 'vue-resource'
+
+
+// 完整引入element
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-default/index.css'
+// Vue.use(ElementUI);
+
+//单独引入
+import { Button, Select } from 'element-ui'
+Vue.use(Button)
+Vue.use(Select)
 
 Vue.config.productionTip = false
+Vue.use(resource);
 
 /* eslint-disable no-new */
 new Vue({
